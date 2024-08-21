f23598e389f165dc2d99f3422087b505f6a5bde8
Abhishek Upadhyay <upadhyayabhishekkumar45@gmail.com>
2024-08-21 09:30:11 +0530

    setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,25 @@
 from setuptools import setup, find_packages
 from typing import List
 
-def 
+HYPEN_E_DOT='-e .'
+
+def get_requirements(file_path:str)->List[str]:
+    requirements = []
+    
+    with open(file_path, 'r') as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace('\n','') for req in requirements]
+        
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+        return requirements    
 
 setup(
     name='DeepSortWithYOLOV5',
     version='0.0.1',
     author='Abhishek Upadhyay',
     author_email='abhishekupadhy9336@gmail.com',
-    packages=find_packages                         
+    install_requires=get_requirements('requirements.txt'),
+    packages=find_packages()                     
 )
