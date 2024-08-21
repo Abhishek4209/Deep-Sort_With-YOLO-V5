db7a97a32144173dca9270ba8b9f5d949718a10d
Abhishek Upadhyay <upadhyayabhishekkumar45@gmail.com>
2024-08-21 08:43:02 +0530

    readme.md

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# Object-Tracking-PyTorch-YOLOv5-DeepSort
+
+This repo represents implimentation of YOLOv5 and DeepSort in Pytorch for object tracking
+
+![alt text](img.gif)
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+from typing import List
+
+def 
+
+setup(
+    name='DeepSortWithYOLOV5',
+    version='0.0.1',
+    author='Abhishek Upadhyay',
+    author_email='abhishekupadhy9336@gmail.com',
+    packages=find_packages                         
+)
